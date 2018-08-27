f8b1747fb012df3700fb03bca2fe47beee053a0b
Roger Dodger <lpaddikt@gmail.com>
2018-08-27 16:30:07 -0700

    Added Greeting Component but with ES6 syntax Part 1

---
+++ src/Greeting.js
@@ -0,0 +1,49 @@
+import React, {Component} from 'react'
+
+const UserGreetings = () => {
+    return <h1>Welcome back!</h1>
+}
+
+const GuestGreeting = () => {
+    return <h1>Please sign up</h1>
+}
+
+class Greeting extends Component {
+    render(){
+        const isLoggedIn = this.props.isLoggedIn;
+        if (isLoggedIn) {
+            return <UserGreetings />;
+        }
+        return <GuestGreeting />
+    }
+    
+}
+
+export default Greeting;
+
+
+
+
+// Using Functions
+
+// function UserGreeting(props) {
+//     return <h1>Welcome back!</h1>;
+//   }
+  
+//   function GuestGreeting(props) {
+//     return <h1>Please sign up.</h1>;
+//   }
+
+//   function Greeting(props) {
+//     const isLoggedIn = props.isLoggedIn;
+//     if (isLoggedIn) {
+//       return <UserGreeting />;
+//     }
+//     return <GuestGreeting />;
+//   }
+  
+//   ReactDOM.render(
+//     // Try changing to isLoggedIn={true}:
+//     <Greeting isLoggedIn={false} />,
+//     document.getElementById('root')
+//   );
--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 //import Clock from './Clock';
 //import App from './App';
-import Toggle from './Toggle';
+// import Toggle from './Toggle';
+import Greeting from './Greeting';
 
 // ReactDOM.render(<App />, document.getElementById('root'));
 //ReactDOM.render(<Clock />, document.getElementById('root'));
-ReactDOM.render(<Toggle />, document.getElementById('root'));
+// ReactDOM.render(<Toggle />, document.getElementById('root'));
+ReactDOM.render(<Greeting isLoggedIn={true}/>, document.getElementById('root'));
