eb6e9bc9b6239e15a6e1eb1b355fefe29e68507a
Roger Dodger <lpaddikt@gmail.com>
2018-08-28 10:12:55 -0700

    The Text Area Tag

--- src/index.js
+++ src/index.js
@@ -6,11 +6,16 @@ import './index.css';
 // import Toggle from './Toggle';
 // import Greeting from './Greeting';
 // import LoginControl from './LoginControl';
-import NameForm from './NameForm';
 
 // ReactDOM.render(<App />, document.getElementById('root'));
 //ReactDOM.render(<Clock />, document.getElementById('root'));
 // ReactDOM.render(<Toggle />, document.getElementById('root'));
 // ReactDOM.render(<Greeting isLoggedIn={true}/>, document.getElementById('root'));
 // ReactDOM.render(<LoginControl />, document.getElementById('root'));
-ReactDOM.render(<NameForm />, document.getElementById('root'));
+
+
+// import NameForm from './NameForm';
+// ReactDOM.render(<NameForm />, document.getElementById('root'));
+
+import EssayForm from './EssayForm';
+ReactDOM.render(<EssayForm />, document.getElementById('root'));
